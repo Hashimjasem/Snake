95e347cf482a5d7b206f550cdeb4df998c6d20f8
Hashimjasem <j_hashim1305@outlook.com>
2023-10-17 18:49:23 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// canvas needs to be created
+    //it should be a grid of 25 x 25
+    // i know that the snake will need to travel in the grid ??
+
+
+
+//create a snake
+    //has a x speed 
+    //has a y speed
+    // it has a size
+    // it stays in the canvas
