63c4cc0f4a1de63d319f833415d8922f05181886
Hashimjasem <j_hashim1305@outlook.com>
2023-10-18 16:54:48 +0800

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-// canvas needs to be created
-    //it should be a grid of 25 x 25
-    // i know that the snake will need to travel in the grid ??
+let grid = document.querySelector(".grid")
+let popup = document.querySelector(".popup");
+let playAgain = document.querySelector(".playAgain");
+let scoreDisplay = document.querySelector(".scoreDisplay");
+let left = document.querySelector(".left");
+let bottom = document.querySelector(".bottom");
+let right = document.querySelector(".right");
+let up = document.querySelector(".top");
+let width = 10;
+let currentIndex = 0;
+let appleIndex = 0;
+let currentSnake = [2, 1, 0];
+let direction = 1;
+let score = 0;
+let speed = 0.8;
+let intervalTime = 0;
+let interval = 0;
 
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("keyup", control);
+    createBoard();
+    startGame();
+    playAgain.addEventListener("click", replay);
+});
 
+function createGrid() {
+    for (i = 0; i < 100; i++) {
+        let div = document.createElement("div")
+        grid.appendChild(div)
+    }
+}
 
-//create a snake
-    //has a x speed 
-    //has a y speed
-    // it has a size
-    // it stays in the canvas
+function startGame() {
+    let squares = document.querySelectorAll(".grid div")
+}
+
