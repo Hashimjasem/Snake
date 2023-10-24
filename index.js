dd2504cc4365ae00f073d8c4c414144cd042865a
Hashimjasem <j_hashim1305@outlook.com>
2023-10-25 00:43:56 +0800

    v4 planning

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 // 1. generate grid
+
 // 2. display snake
+//add class depending on the value of current snake
+
 // 3. move snake
+// update value of current snake every interval
+// remove tail
+// add head
+// head value will be calculated based on a direction value
+
+
+
+
+
 // 4. spawn apple
+//will randomly spawn in a square on the grid
 // 5. apple makes snake longer
+//multiple ways of acheiving this
+
 // 6. if snake hits something game over
